8effe7058aeeb7e3c3ab3a91feaea1594fa47abd
Nikolas Konstantakopoulos <n.konstantakopoulos@indice.gr>
2024-04-05 15:52:40 +0300

    Biometric - user canceled error differentiation

--- Package.swift
+++ Package.swift
@@ -7,7 +7,6 @@ let package = Package(
     name: "Indice.Identity",
     platforms: [.iOS(.v13), .macOS(.v10_15)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "IndiceIdentity",
             targets: ["Indice.Identity"]),
@@ -16,8 +15,6 @@ let package = Package(
         .package(url: "https://github.com/indice-co/Indice.Swift.Networking", .upToNextMajor(from: "1.2.3"))
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Indice.Identity",
             dependencies: [
